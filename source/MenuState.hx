30e8220b8d9b6afbbca1ae9b4dd30a4538659724
Carlos Saúl Menem <pablopalacios@maildrop.cc>
2016-09-02 17:16:54 -0300

    Segundo commit
    Ñañañañañ

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -3,15 +3,14 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.ui.FlxButton;
-import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
+		Reg.highscore = 1000;
+		Reg.highscore += Reg.puntosEnemigo;
 	}
 
 	override public function update(elapsed:Float):Void
---
+++ source/Reg.hx
@@ -0,0 +1,13 @@
+package;
+
+class Reg
+{	
+	static public var highscore:Int = 0;
+	static public var cantVidas:Int = 3;
+	static public var nombreHighscore:String = "AFC";
+	
+	inline static public var maxCantVidas = 3;
+	inline static public var velDisparo = 200;
+	inline static public var puntosEnemigo = 100;
+	inline static public var puntosOvni = 1000;
+}
